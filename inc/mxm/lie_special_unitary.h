912db6fac7e09cc450a077feb0944d9f6554a66f
XiaoxingChen <chen-xx@zju.edu.cn>
2021-10-10 11:40:30 +0800

    Modify: fix bug in su(2).

--- inc/mxm/lie_special_unitary.h
+++ inc/mxm/lie_special_unitary.h
@@ -17,8 +17,19 @@ template<typename DType>
 bool isValid(const Matrix<Complex<DType>>& mat, DType* error=nullptr, DType tol=std::numeric_limits<DType>::epsilon())
 {
     if(! isSquare(mat)) return false;
-    return isZero(mat - conj(mat.T()), tol, error) && norm(mat - DType(1)) < tol;
+    return isZero(mat + conj(mat.T()), tol, error);
 }
+#if 0
+template <size_t N, typename DeriveType>
+
+exp(const MatrixBase<DeriveType>& skew)
+{
+    using DType = typename Traits<DeriveType>::EntryType;
+
+    return Matrix<DType>::identity(N);
+}
+#endif
+
 } // namespace su
 
 namespace SU
@@ -27,7 +38,7 @@ template<typename DType>
 bool isValid(const Matrix<Complex<DType>>& mat, DType tol=std::numeric_limits<DType>::epsilon(), DType* error=nullptr)
 {
     if(! isSquare(mat)) return false;
-    return isIdentity(mat.matmul(conj(mat.T())), error, tol);
+    return isIdentity(mat.matmul(conj(mat.T())), error, tol) && norm(mxm::det(mat) - DType(1)) < tol;
 }
 
 #if 0
