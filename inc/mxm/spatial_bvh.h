73b9579b38b4770f6967daaa2924876f50e40402
XiaoxingChen <chen-xx@zju.edu.cn>
2023-02-10 21:32:29 +0800

    optimize: make bvh tree template.

--- inc/mxm/spatial_bvh.h
+++ inc/mxm/spatial_bvh.h
@@ -3,6 +3,7 @@
 
 #include "spatial_aabb.h"
 #include "geometry_primitive.h"
+#include "spatial_tree_base.h"
 #include <stack>
 #include <memory>
 #include <map>
@@ -22,30 +23,32 @@ enum HitType
     eHitTypeNum
 };
 
+template<typename DType=float>
 struct Node
 {
-    Node(size_t dim): aabb(dim), is_leaf(false){}
-    AABB<float> aabb;
+    Node<DType>(size_t dim): aabb(dim), is_leaf(false){}
+    AABB<DType> aabb;
     std::vector<size_t> children_index_buffer;
     std::vector<size_t> primitive_index_buffer;
     bool is_leaf;
     size_t prim_num;
 };
 
+template<typename DType=float>
 class BaseTree
 {
 public:
     virtual size_t dim() const = 0;
-    virtual Mat primitive(size_t idx) const = 0;
+    virtual Matrix<DType> primitive(size_t idx) const = 0;
     virtual size_t primitiveSize() const = 0;
     virtual void build(size_t primitive_per_leaf=4, bool verbose=true);
-    const std::vector<bvh::Node>& nodeBuffer() const { return node_buffer_;}
+    const std::vector<bvh::Node<DType>>& nodeBuffer() const { return node_buffer_;}
 
 protected:
-    std::vector<bvh::Node> node_buffer_;
+    std::vector<bvh::Node<DType>> node_buffer_;
 };
 
-class PrimitiveMeshTree: public BaseTree
+class PrimitiveMeshTree: public BaseTree<>
 {
 public:
     struct HitRecord
@@ -95,7 +98,7 @@ size_t rayCast(
     Vector<size_t>* p_prim_idx=nullptr,
     Vector<float>* p_hit_t=nullptr);
 
-class PointCloudTree: public BaseTree
+class PointCloudTree: public BaseTree<>
 {
 public:
     PointCloudTree(
@@ -115,6 +118,102 @@ private:
     std::shared_ptr<Mat> point_buffer_;
     // std::vector<bvh::Node> node_buffer_;
 };
+
+size_t treeNodeRequirement(size_t leaf_num, size_t child_num);
+bool verifyTree(const std::vector<bvh::Node<>>& node_buffer, size_t node_idx);
+
+template<typename DType>
+void BaseTree<DType>::build(size_t primitive_per_leaf, bool verbose)
+{
+    std::vector<size_t> primitive_index_buffer(primitiveSize());
+    for(size_t i = 0; i < primitive_index_buffer.size(); i++) primitive_index_buffer.at(i) = i;
+
+    {
+        size_t require = treeNodeRequirement(primitive_index_buffer.size() / primitive_per_leaf + 1, 2);
+        node_buffer_.clear();
+        node_buffer_.reserve(require);
+    }
+
+    std::stack<RangeNode> stk;
+
+    stk.push({node_buffer_.size(), {0, primitive_index_buffer.size()}});
+    node_buffer_.push_back(Node(dim()));
+
+    auto t_start = std::chrono::system_clock::now();
+    while (!stk.empty())
+    {
+        auto target = stk.top();
+        stk.pop();
+
+        auto& target_node = node_buffer_.at(target.node_idx);
+        target_node.prim_num = target.range[1] - target.range[0];
+
+        //
+        // eliminate leaf node
+        if(target.range[1] - target.range[0] <= primitive_per_leaf)
+        {
+            for(size_t sorted_idx = target.range[0]; sorted_idx < target.range[1]; sorted_idx++)
+            {
+                auto prim_idx = primitive_index_buffer.at(sorted_idx);
+                target_node.primitive_index_buffer.push_back(prim_idx);
+                target_node.aabb.extend( primitive(prim_idx) );
+            }
+
+            target_node.is_leaf = true;
+            continue;
+        }
+
+        //
+        // deal with internal node
+        //
+
+        //
+        // calculate AABB
+        for(size_t i = target.range[0]; i < target.range[1]; i++)
+        {
+            const size_t& prim_idx = primitive_index_buffer.at(i);
+            target_node.aabb.extend(primitive(prim_idx));
+        }
+
+        //
+        // find the longest axis
+        size_t target_axis = argMax(target_node.aabb.max() - target_node.aabb.min())[0];
+        if(verbose)
+            std::cout << "range: [" << target.range[0] << "," << target.range[1] << "), axis: " << target_axis << std::endl;
+
+        //
+        // sort along the target axis
+        size_t mid = (target.range[1] + target.range[0]) / 2;
+
+        std::nth_element(
+            primitive_index_buffer.begin() + target.range[0],
+            primitive_index_buffer.begin() + mid,
+            primitive_index_buffer.begin() + target.range[1],
+            [&](const size_t& prim_idx1, const size_t& prim_idx2)
+            {
+                Mat prim1 = primitive(prim_idx1);
+                Mat prim2 = primitive(prim_idx2);
+
+                return mxm::sum(prim1(Row(target_axis))) < mxm::sum(prim2(Row(target_axis)));
+            }
+        );
+
+        // create children
+
+        stk.push({node_buffer_.size(), {target.range[0], mid}});
+        target_node.children_index_buffer.push_back(node_buffer_.size());
+        node_buffer_.push_back(Node(dim()));
+
+        stk.push({node_buffer_.size(), {mid, target.range[1]}});
+        target_node.children_index_buffer.push_back(node_buffer_.size());
+        node_buffer_.push_back(Node(dim()));
+
+    }
+    auto t_end = std::chrono::system_clock::now();
+    std::cout << "build tree t_cost(s): " << std::chrono::duration<double>(t_end - t_start).count() << std::endl;
+    if(!verifyTree(node_buffer_, 0))
+        throw std::runtime_error(std::string(__FILE__) + ":" + std::to_string(__LINE__));
+}
 
 
 } // namespace bvh
--- inc/mxm/spatial_bvh_inl.h
+++ inc/mxm/spatial_bvh_inl.h
@@ -31,7 +31,7 @@ MXM_INLINE size_t treeNodeRequirement(size_t leaf_num, size_t child_num=2)
     return (1 << height);
 }
 
-MXM_INLINE bool verifyTree(const std::vector<bvh::Node>& node_buffer, size_t node_idx)
+MXM_INLINE bool verifyTree(const std::vector<bvh::Node<>>& node_buffer, size_t node_idx)
 {
     if(node_buffer.at(node_idx).is_leaf)
         return true;
@@ -43,98 +43,6 @@ MXM_INLINE bool verifyTree(const std::vector<bvh::Node>& node_buffer, size_t nod
             return false;
     }
     return true;
-}
-
-MXM_INLINE void BaseTree::build(size_t primitive_per_leaf, bool verbose)
-{
-    std::vector<size_t> primitive_index_buffer(primitiveSize());
-    for(size_t i = 0; i < primitive_index_buffer.size(); i++) primitive_index_buffer.at(i) = i;
-
-    {
-        size_t require = treeNodeRequirement(primitive_index_buffer.size() / primitive_per_leaf + 1);
-        node_buffer_.clear();
-        node_buffer_.reserve(require);
-    }
-
-    std::stack<RangeNode> stk;
-
-    stk.push({node_buffer_.size(), {0, primitive_index_buffer.size()}});
-    node_buffer_.push_back(Node(dim()));
-
-    auto t_start = std::chrono::system_clock::now();
-    while (!stk.empty())
-    {
-        auto target = stk.top();
-        stk.pop();
-
-        auto& target_node = node_buffer_.at(target.node_idx);
-        target_node.prim_num = target.range[1] - target.range[0];
-
-        //
-        // eliminate leaf node
-        if(target.range[1] - target.range[0] <= primitive_per_leaf)
-        {
-            for(size_t sorted_idx = target.range[0]; sorted_idx < target.range[1]; sorted_idx++)
-            {
-                auto prim_idx = primitive_index_buffer.at(sorted_idx);
-                target_node.primitive_index_buffer.push_back(prim_idx);
-                target_node.aabb.extend( primitive(prim_idx) );
-            }
-
-            target_node.is_leaf = true;
-            continue;
-        }
-
-        //
-        // deal with internal node
-        //
-
-        //
-        // calculate AABB
-        for(size_t i = target.range[0]; i < target.range[1]; i++)
-        {
-            const size_t& prim_idx = primitive_index_buffer.at(i);
-            target_node.aabb.extend(primitive(prim_idx));
-        }
-
-        //
-        // find the longest axis
-        size_t target_axis = argMax(target_node.aabb.max() - target_node.aabb.min())[0];
-        if(verbose)
-            std::cout << "range: [" << target.range[0] << "," << target.range[1] << "), axis: " << target_axis << std::endl;
-
-        //
-        // sort along the target axis
-        size_t mid = (target.range[1] + target.range[0]) / 2;
-
-        std::nth_element(
-            primitive_index_buffer.begin() + target.range[0],
-            primitive_index_buffer.begin() + mid,
-            primitive_index_buffer.begin() + target.range[1],
-            [&](const size_t& prim_idx1, const size_t& prim_idx2)
-            {
-                Mat prim1 = primitive(prim_idx1);
-                Mat prim2 = primitive(prim_idx2);
-
-                return mxm::sum(prim1(Row(target_axis))) < mxm::sum(prim2(Row(target_axis)));
-            }
-        );
-
-        // create children
-
-        stk.push({node_buffer_.size(), {target.range[0], mid}});
-        target_node.children_index_buffer.push_back(node_buffer_.size());
-        node_buffer_.push_back(Node(dim()));
-
-        stk.push({node_buffer_.size(), {mid, target.range[1]}});
-        target_node.children_index_buffer.push_back(node_buffer_.size());
-        node_buffer_.push_back(Node(dim()));
-
-    }
-    auto t_end = std::chrono::system_clock::now();
-    std::cout << "build tree t_cost(s): " << std::chrono::duration<double>(t_end - t_start).count() << std::endl;
-    if(!verifyTree(node_buffer_, 0))
-        throw std::runtime_error(std::string(__FILE__) + ":" + std::to_string(__LINE__));
 }
 
 MXM_INLINE size_t PrimitiveMeshTree::multiHit(const Ray<>& ray) const
@@ -163,7 +71,7 @@ MXM_INLINE size_t rayCast(
 
     while(! node_idx_stk.empty())
     {
-        const Node& target_node = tree.nodeBuffer().at(node_idx_stk.top());
+        const Node<>& target_node = tree.nodeBuffer().at(node_idx_stk.top());
         node_idx_stk.pop();
 
         //
@@ -246,7 +154,7 @@ MXM_INLINE std::vector<PrimitiveMeshTree::HitRecord> PrimitiveMeshTree::hit(cons
 
 MXM_INLINE std::multimap<FloatType, size_t> PointCloudTree::radiusSearch(const Vec& pt, FloatType radius) const
 {
-    std::stack<Node> stk;
+    std::stack<Node<>> stk;
     stk.push(nodeBuffer().at(0));
 
     std::multimap<FloatType, size_t> ret;
